f6d82ef28fed6f76056d8caeee34776b505a256f
Rachel Kim <yek99kr@gmal.com>
2022-02-24 15:35:28 -0500

    a

--- component/AboutMadlibWord3.js
+++ component/AboutMadlibWord3.js
@@ -2,22 +2,22 @@ import { useState, useEffect } from "react";
 import useMouse from "../hooks/useMouse";
 
 const AboutMadlibWord3 = ({ textColor, madWords }) => {
-  const { x, y, mx, my } = useMouse();
-
   const [mad, setMad] = useState(madWords[0]);
 
-  useEffect(() => {
-    setMad(madWords[Math.floor(Math.random() * 2)]);
-  }, [mx]);
+  const { x, y, mx, my } = useMouse();
 
   const [sTime, setSTime] = useState(mx);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setSTime(mx);
-    }, 16);
+    }, 14);
     return () => clearInterval(interval);
   }, [mx]);
+
+  useEffect(() => {
+    setMad(madWords[Math.floor(Math.random() * 2)]);
+  }, [sTime]);
 
   return (
     <span>
