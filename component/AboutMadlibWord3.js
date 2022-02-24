546961bc763ada6f2135b0fdb55525cd181acc85
Rachel Kim <yek99kr@gmal.com>
2022-02-24 15:08:16 -0500

    lol

--- component/AboutMadlibWord3.js
+++ component/AboutMadlibWord3.js
@@ -1,36 +1,23 @@
 import { useState, useEffect } from "react";
+import useMouse from "../hooks/useMouse";
 
-const AboutMadlibWord3 = ({ textColor, madWords, mx }) => {
+const AboutMadlibWord3 = ({ textColor, madWords }) => {
+  const { x, y, mx, my } = useMouse();
+
   const [mad, setMad] = useState(madWords[0]);
-  // const [dropdownColor1, setdropdownColor1] = useState("text-black");
-  // const [dropdownColor2, setdropdownColor2] = useState("text-black");
-  // const [dropdownColor3, setdropdownColor3] = useState("text-black");
-  // setInterval(() => {
-  //   // Your custom logic here
-  //   setMad(madWords[Math.floor(Math.random() * 2)]);
-  // }, 1000);
 
   useEffect(() => {
-    // eslint-disable-line
     setMad(madWords[Math.floor(Math.random() * 2)]);
   }, [mx]);
 
-  // useEffect(() => {
-  //   if (mx === 0) return;
-  //   setChangeTime(500);
-  // }, [mx]);
+  const [sTime, setSTime] = useState(mx);
 
-  // useEffect(() => {
-  //   // if (mx === 0 || !mx) return;
-  //   console.log(mx);
-  //   // console.log((2 / (Math.abs(mx) + 1).toFixed(1)) * 500);
-  //   if (mx === 0 || mx === null) return;
-
-  //   const interval = setInterval(() => {
-  //     setMad(madWords[Math.floor(Math.random() * 2)]);
-  //   }, 100);
-  //   return () => clearInterval(interval);
-  // }, [mx]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSTime(mx);
+    }, 14);
+    return () => clearInterval(interval);
+  }, [mx]);
 
   return (
     <span>
--- pages/about3.js
+++ pages/about3.js
@@ -3,27 +3,27 @@ import { useState, useEffect } from "react";
 // import useMouse from "../hooks/useMouse";
 
 const about3 = () => {
-  const [mPosition, setMPosition] = useState({
-    x: null,
-    y: null,
-    mx: null,
-    my: null,
-  });
+  // const [mPosition, setMPosition] = useState({
+  //   x: null,
+  //   y: null,
+  //   mx: null,
+  //   my: null,
+  // });
 
-  useEffect(() => {
-    function usehandle(e) {
-      setMPosition({
-        x: e.pageX,
-        y: e.pageY,
-        mx: e.movementX,
-        my: e.movementY,
-      });
-    }
-    document.addEventListener("mousemove", handle);
-    return () => document.removeEventListener("mousemove", usehandle);
-  });
+  // useEffect(() => {
+  //   function usehandle(e) {
+  //     setMPosition({
+  //       x: e.pageX,
+  //       y: e.pageY,
+  //       mx: e.movementX,
+  //       my: e.movementY,
+  //     });
+  //   }
+  //   document.addEventListener("mousemove", handle);
+  //   return () => document.removeEventListener("mousemove", usehandle);
+  // });
 
-  const mx = mPosition.mx;
+  // const mx = mPosition.mx;
 
   const madWords1 = ["industry leading", "plant based", "future proof"];
   const madWords2 = ["exploring", "inspiring", "navigating"];
@@ -38,15 +38,6 @@ const about3 = () => {
   const madWords11 = ["educate", "laugh", "communicate"];
   const madWords12 = ["convert", "emote", "love"];
 
-  const [sTime, setSTime] = useState(mx);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setSTime(mx);
-    }, 14);
-    return () => clearInterval(interval);
-  }, [mx]);
-
   return (
     <div className="bg-gray-200 h-[100vh] w-full">
       <p className="font-[600] text-[6.2vw] leading-[1.15] pt-2 pr-3 pl-6 ">
@@ -54,74 +45,27 @@ const about3 = () => {
         <AboutMadlibWord3
           textColor={"text-[#2a94ff]"}
           madWords={madWords1}
-          mx={sTime}
         />{" "}
         creative studio{" "}
-        <AboutMadlibWord3
-          textColor={"text-[#ffc128]"}
-          madWords={madWords2}
-          mx={sTime}
-        />{" "}
+        <AboutMadlibWord3 textColor={"text-[#ffc128]"} madWords={madWords2} />{" "}
         the{" "}
-        <AboutMadlibWord3
-          textColor={"text-[#ee341b]"}
-          madWords={madWords3}
-          mx={sTime}
-        />{" "}
+        <AboutMadlibWord3 textColor={"text-[#ee341b]"} madWords={madWords3} />{" "}
         between{" "}
-        <AboutMadlibWord3
-          textColor={"text-[#2a94ff]"}
-          madWords={madWords4}
-          mx={sTime}
-        />{" "}
+        <AboutMadlibWord3 textColor={"text-[#2a94ff]"} madWords={madWords4} />{" "}
         and{" "}
-        <AboutMadlibWord3
-          textColor={"text-[#ffc128]"}
-          madWords={madWords5}
-          mx={sTime}
-        />
-        . We{" "}
-        <AboutMadlibWord3
-          textColor={"text-[#ee341b]"}
-          madWords={madWords6}
-          mx={sTime}
-        />{" "}
-        <AboutMadlibWord3
-          textColor={"text-[#2a94ff]"}
-          madWords={madWords7}
-          mx={sTime}
-        />{" "}
+        <AboutMadlibWord3 textColor={"text-[#ffc128]"} madWords={madWords5} />.
+        We{" "}
+        <AboutMadlibWord3 textColor={"text-[#ee341b]"} madWords={madWords6} />{" "}
+        <AboutMadlibWord3 textColor={"text-[#2a94ff]"} madWords={madWords7} />{" "}
         and{" "}
-        <AboutMadlibWord3
-          textColor={"text-[#ffc128]"}
-          madWords={madWords8}
-          mx={sTime}
-        />{" "}
+        <AboutMadlibWord3 textColor={"text-[#ffc128]"} madWords={madWords8} />{" "}
         for{" "}
-        <AboutMadlibWord3
-          textColor={"text-[#ee341b]"}
-          madWords={madWords9}
-          mx={sTime}
-        />{" "}
+        <AboutMadlibWord3 textColor={"text-[#ee341b]"} madWords={madWords9} />{" "}
         that{" "}
-        <AboutMadlibWord3
-          textColor={"text-[#2a94ff]"}
-          madWords={madWords10}
-          mx={sTime}
-        />
-        ,{" "}
-        <AboutMadlibWord3
-          textColor={"text-[#ffc128]"}
-          madWords={madWords11}
-          mx={sTime}
-        />
-        , and{" "}
-        <AboutMadlibWord3
-          textColor={"text-[#ee341b]"}
-          madWords={madWords12}
-          mx={sTime}
-        />
-        .
+        <AboutMadlibWord3 textColor={"text-[#2a94ff]"} madWords={madWords10} />,{" "}
+        <AboutMadlibWord3 textColor={"text-[#ffc128]"} madWords={madWords11} />,
+        and{" "}
+        <AboutMadlibWord3 textColor={"text-[#ee341b]"} madWords={madWords12} />.
       </p>
     </div>
   );
